02c4937484b85f4b11c6e71a59e728eef04657ef
Jaron-Jiang <2896247746@qq.com>
2022-06-27 11:07:57 +0800

    create server

---
+++ ProjectAnalysis/src/test/java/cn/sunist/projectanalysis/ProjectAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.sunist.projectanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
